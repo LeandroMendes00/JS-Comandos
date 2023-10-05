20d88049ab619f6f116e6165ab9cbf4da209ec4d
Leandro Alberto Santana Mendes <91228406+lmendes2001@users.noreply.github.com>
2023-10-05 18:41:24 -0300

    Comando e suas funções

---
+++ ATALHO.js
@@ -0,0 +1,157 @@
+// 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 ATT
+// 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 04/05/2023 ATT
+/*
+    *
+        *
+            A
+                U
+                    L
+                        A
+                            6
+                                *
+                                    *
+                                        */
+// Não pode começar o nome de uma variável com um número
+/*
+Exemplo
+let no1me;
+*/
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Ex: let nomeCompletoDoCliente= 'Leandro';
+//Case-sensitive
+//Não podemos redeclarar variáveis com let
+//NÃO UTILIZE VAR, UTILIZE LET.
+
+/*
+Exemplo
+let nomeCliente = 'Leandro';
+nomeCliente = 'Leandro';
+console.log(nomeCliente);
+*/
+
+
+/*
+    *
+        *
+            A
+                U
+                    L
+                         A
+                            7
+                                *
+                                    *
+                                        */
+
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constantes com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constantes 
+// NÃO UTILIZE VAR, UTILIZE LET.
+/*
+    *
+        *
+            A
+                U
+                    L
+                        A
+                            1
+                                0
+                                    *
+                                        *
+                                            */                                           
+/* Aritméticos
+* + Adição / Concatenação( - / * )
+* ** Potenciação
+* % Resto da divisão
+* Ordem
+()
+**
+* / %
+* -
+
+    Incremento = ++
+    Decremente = --
+
+        //NaN-  Not a number parseInt (inteiro), parseFloat(decimais)
+
+*/
+/*
+    *
+        *
+            A
+                U
+                    L
+                        A
+                            1
+                                3
+                                    *
+                                        *
+                                            */   
+/*
+
+console.log(umaString.chaAt())
+O método charAt()retorna o caractere especificado a partir de uma string.
+
+console.log(umaString.match())
+O método match() retorna uma correspondência entre uma string com uma expressão regular.
+
+console.log(umaString.search())
+O método search() realiza uma busca por uma ocorrência entre uma expressão regular e uma
+
+console.log(umaString.concat())
+O método concat()combina o texto de duas ou mais strings e retorna uma nova string.
+
+console.log(umaString.indexOf())
+O método indexOf()retorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente.
+
+console.log(umaString.lastIndexOf())
+O método lastIndexOf()retorna o último índice que um certo elemento pode ser encontrado em nenhum array, ou -1 se o elemento não estiver presente. O array é pesquisado de trás para frente, começando pelo fromIndex.
+
+console.log(umaString.replace())
+O método replace()retorna uma nova string com algumas ou todas as correspondências de um padrão substituído por um determinado caractere (ou caracteres). O padrão pode ser uma string ou uma RegExp, e a substituição pode ser uma string ou uma função a ser chamada para cada correspondência. Se o padrão for uma string, apenas a primeira ocorrência será substituída.
+
+console.log(umaString.length())
+A propriedade lengthde um objeto Stringcontém o comprimento da string. lengthé uma propriedade read-only(alguma leitura) de instâncias de string.
+
+console.log(umaString.slice())
+O método slice()retorna uma cópia de parte de um array a partir de um subarray criado entre as posições inícioe fim(fim não é incluído) de um array original. O Array original não é modificado.
+
+console.log(umaString.substring())
+O método substring() retorna a parte da string entre os índices inicial e final, ou até o final da string.
+
+console.log(umaString.split())
+O método split()divide uma Stringem uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array. A divisão é feita procurando um padrão, onde o padrão é fornecido como o primeiro parâmetro na chamada do método.
+
+console.log(umaString.toUpperCase ou toLowerCase())
+O método toUpperCase()retorna o valor da string original convertida em letras maiúsculas e minúsculas.
+
+
+/***************************            https://developer.mozilla.org/en-US/            ********************************/
+
+/* 
+PRECEDÊNCIA
+
+()
+**
+* / %
++ -
+
+INCREMENTO 
+
+ x += 10
+*/
+
+
+/*
+    *
+        ********************
+            * REVISÂO ARRAYS    AULA 62@@
+              ***************
+                                *
+                                    */ 
+
+                                          
