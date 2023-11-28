43e4891e4f78b6d6608395b69862ff9d56336daa
Leandro Alberto Santana Mendes <91228406+LeandroMendes00@users.noreply.github.com>
2023-11-28 18:29:49 -0300

    Add files via upload

---
+++ JavaScript Projeto Conceitos.js
@@ -0,0 +1,71 @@
+// /* Conceitos Fundamentais */
+    
+
+// /* Variáveis */
+
+// let nome = "João";
+// const idade = 25;
+
+
+// /* Tipos de Dados */
+// let numero = 42;
+// let texto = "Olá, mundo!";
+// let verdadeiroOuFalso = true;
+// let lista = [1, 2, 3];
+// let objeto = { chave: "valor" };
+
+
+// /* Operadores */
+// let soma = 5 + 3;
+// let concatenacao = "Olá" + " " + "mundo";
+// let igualdade = 10 === 5;
+
+
+// /* Controle de Fluxo */
+// if (idade >= 18) {
+//     console.log("Você é maior de idade.");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+// /* Funções */
+// function saudacao(nome) {
+//     return "Olá, " + nome + "!";
+// }
+
+// let mensagem = saudacao("Maria");
+// console.log(mensagem);
+
+
+// /* Objetos e Arrays */
+// let carro = {
+//     modelo: "Fusca",
+//     ano: 1970,
+//     cor: "azul"
+// };
+
+// let frutas = ["maçã", "banana", "laranja"];
+
+
+// /* Eventos */
+// let botao = document.getElementById("meuBotao");
+
+// botao.addEventListener("click", function() {
+//     console.log("Botão clicado!");
+// });
+
+
+// /* DOM (Document Object Model) */
+// let elemento = document.getElementById("meuElemento");
+// elemento.innerHTML = "Novo conteúdo";
+
+
+
+
+
+
